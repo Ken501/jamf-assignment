12852d0a83e2d4a2ca1514e26f6c0c31227f1f9b
kmadmin <kenneth_young501@live.com>
2023-08-12 11:36:55 -0400

    Modified data resource blocks

--- helm/ingress-controller/data.tf
+++ helm/ingress-controller/data.tf
@@ -1,10 +1,10 @@
 // EKS Cluster Data Resources
 
 data "aws_eks_cluster" "eks_cluster" {
-  name = "${var.environment}-${var.app_name}-cluster-${module.Global-Vars.location}"
+  name = "${var.environment}-${var.app_name}-cluster-${module.global-vars.location}"
 }
 
 data "aws_eks_cluster_auth" "cluster_auth" {
-  name = "${var.environment}-${var.app_name}-cluster-${module.Global-Vars.location}"
+  name = "${var.environment}-${var.app_name}-cluster-${module.global-vars.location}"
 
 }
