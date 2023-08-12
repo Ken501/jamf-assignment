d2140383a1c2c7cb3fe97991904cbe1e546db3d7
kmadmin <kenneth_young501@live.com>
2023-08-12 15:29:50 -0400

    Modified set

--- helm/wordpress/wordpress.tf
+++ helm/wordpress/wordpress.tf
@@ -10,66 +10,66 @@ resource "helm_release" "wordpress" {
   set {
     name  = "replicaCount"
     value = "2"
-  },
+  }
 
   set {
     name  = "service.type"
     value = "NodePort"
-  },
+  }
 
   set {
     name  = "ingress.enabled"
     value = true
-  },
+  }
 
   set {
     name  = "ingress.ingressClassName"
     value = "alb"
-  },
+  }
 
   set {
     name  = "ingress.hostname"
     value = "wordpress.kmartinez.net"
-  },
+  }
 
   set {
     name  = "ingress.annotations"
     value = "alb.ingress.kubernetes.io/certificate-arn: ${module.global-vars.wildcard_certificate}"
-  },
+  }
 
   set {
     name  = "ingress.annotations"
     value = "alb.ingress.kubernetes.io/scheme: internet-facing"
-  },
+  }
 
   set {
     name  = "ingress.annotations"
     value = "alb.ingress.kubernetes.io/load-balancer-name: ${var.environment}-${var.app_name}-external-alb-${module.global-vars.location}"
-  },
+  }
 
   set {
     name  = "ingress.annotations"
     value = "alb.ingress.kubernetes.io/load-balancer-attributes: deletion_protection.enabled=false"
-  },
+  }
 
   set {
     name  = "ingress.annotations"
     value = "alb.ingress.kubernetes.io/ip-address-type: ipv4"
-  },
+  }
 
   set {
     name  = "ingress.annotations"
     value = "alb.ingress.kubernetes.io/tags: Environment=${var.environment},Owner=${var.owner},App=${var.app_name}"
   }
 
-  # set {
-  #   name  = "ingress.annotations"
-  #   value = "alb.ingress.kubernetes.io/listen-ports: '[{"HTTP": 80}, {"HTTPS": 443}]'"
-  # }
+  set {
+    name  = "ingress.annotations"
+    value = "alb.ingress.kubernetes.io/listen-ports: '[{"HTTP": 80},{"HTTPS": 443}]'"
+  }
 
-  # set {
-  #   name  = "ingress.annotations"
-  #   value = "alb.ingress.kubernetes.io/subnets: ${module.global-vars.subnet_id_map["public1"]}, ${module.global-vars.subnet_id_map["public2"]}"
-  # }
+  set {
+    name  = "ingress.annotations"
+    value = "alb.ingress.kubernetes.io/subnets: ${module.global-vars.subnet_id_map["public1"]},${module.global-vars.subnet_id_map["public2"]}"
+  }
 
 }
