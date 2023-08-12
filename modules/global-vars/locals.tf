1ef6f4be1a559aa118d8086c27c019998093a094
kmadmin <kenneth_young501@live.com>
2023-08-12 10:42:17 -0400

    Update network vpc

--- modules/global-vars/locals.tf
+++ modules/global-vars/locals.tf
@@ -8,8 +8,8 @@ locals {
 
   vpc_id_map = {
     us-east-1 = {
-      "test" = "vpc-0905e9730c5444ab5" # Allows allocation of multiple VPC IDs for multiple environments
-      "dev"  = "vpc-0905e9730c5444ab5"
+      "test" = "vpc-0905d63e32f01c876" # Allows allocation of multiple VPC IDs for multiple environments
+      "dev"  = "vpc-0905d63e32f01c876"
     }
   }
   vpc_id = "${local.vpc_id_map[var.AWS_REGION]}"
